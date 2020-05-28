178708555edae868e68c62177f2f4c7de50f473d
lucas clayson <lucas.sant@unigranrio.br>
2020-05-28 09:15:42 -0300

    commit 12 primeira mudança

--- jss/menu.js
+++ jss/menu.js
@@ -1,9 +1,19 @@
 $(function() {
     $(window).scroll(function(){
-        if ($(this).scrollTop() > 240){
-            $("#menu").addClass("menu-fixed-top", 500);
+        if ($(this).scrollTop() > 80){
+            $("#menu").addClass("menu-fixed-top img", 500);
+            $("#menu2").addClass("menu2-2", 500);
+            $(".logo").addClass("logo2", 500);
+            $(".display").addClass("display2", 500);
+            $(".teste").addClass("test2", 500);
+            $("#fundo-logo").addClass("fundo-logo2", 500);
         } else {
             $("#menu").removeClass("menu-fixed-top", 500);
+            $("#menu2").removeClass("menu2-2", 500);
+            $(".logo").removeClass("logo2", 500);
+            $(".display").removeClass("display2", 500);
+            $(".teste").removeClass("test2", 500);
+            $("#fundo-logo").removeClass("fundo-logo2", 500);
         }
     });
 });
