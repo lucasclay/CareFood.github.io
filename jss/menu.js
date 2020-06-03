3861327a642026904794f73c937b3061873b106e
lucas clayson <lucas.sant@unigranrio.br>
2020-06-02 22:06:03 -0300

    commit 13

--- jss/menu.js
+++ jss/menu.js
@@ -1,19 +1,27 @@
 $(function() {
     $(window).scroll(function(){
-        if ($(this).scrollTop() > 80){
-            $("#menu").addClass("menu-fixed-top img", 500);
-            $("#menu2").addClass("menu2-2", 500);
-            $(".logo").addClass("logo2", 500);
-            $(".display").addClass("display2", 500);
-            $(".teste").addClass("test2", 500);
-            $("#fundo-logo").addClass("fundo-logo2", 500);
+        if ($(this).scrollTop() > 140){
+            $("#menu").addClass("menu-total", 5000);
+            $("#menu-ul").addClass("menu-ul2", 5000);
+            $(".menu-li").addClass("menu-li2", 5000);
+            $(".menu-div").addClass("menu-div2", 5000);
+            $(".menu-div").addClass("menu-div2:hover", 5000);
+            $(".logo").addClass("logo2", 5000);
+            $("#fundo-logo").addClass("fundo-logo2", 5000);
+           //$("#up").addClass("up2", 500);
         } else {
-            $("#menu").removeClass("menu-fixed-top", 500);
-            $("#menu2").removeClass("menu2-2", 500);
-            $(".logo").removeClass("logo2", 500);
-            $(".display").removeClass("display2", 500);
-            $(".teste").removeClass("test2", 500);
-            $("#fundo-logo").removeClass("fundo-logo2", 500);
+            $("#menu").removeClass("menu-total", 5000);
+            $("#menu-ul").removeClass("menu-ul2", 5000);
+            $(".menu-li2").removeClass("menu-li2", 5000);
+            $(".menu-div").removeClass("menu-div2", 5000);
+            $(".logo").removeClass("logo2", 5000);
+            $("#fundo-logo").removeClass("fundo-logo2", 5000);
+            //$("#up").removeClass("up2", 500);
+        }
+        if ($(this).scrollTop() > 500){
+            $("#up").addClass("up2", 5);
+        } else {
+            $("#up").removeClass("up2", 5);
         }
     });
 });
